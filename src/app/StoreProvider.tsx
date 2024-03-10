df0750e2b7e0964caf2d4a30d8ec949b8e47a739
foreverfl <forever_fl@naver.com>
2024-03-10 20:08:47 +0900

    Remove cache

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "../lib/store";
 
-export default function Storeprovider({
+export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
