589b4d80cc7a86948e8b432f6b78d3dea9b570ca
foreverfl <forever_fl@naver.com>
2024-03-10 18:32:47 +0900

    Modify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,27 @@
 /** @type {import('next').NextConfig} */
-import { fileURLToPath } from 'url';
+
 import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export const images = {
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'avatars.githubusercontent.com',
-        },
-    ],
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https', // 이미지 URL의 프로토콜
+                hostname: 'avatars.githubusercontent.com', // 허용할 외부 이미지 호스트 명
+            }
+        ],
+    },
+    webpack: (config) => {
+        // Alias 설정 추가
+        config.resolve.alias['@'] = join(__dirname, 'src');
+        config.resolve.alias['@app'] = join(__dirname, 'src/app');
+
+        return config;
+    },
 };
 
-export function webpack(config) {
-    config.resolve.alias['@'] = join(__dirname, 'src');
-    return config;
-}
+
+export default nextConfig;
