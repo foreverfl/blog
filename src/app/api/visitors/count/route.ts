a7e7e04053ae043673ed6c7f15a674f7d25e656b
foreverfl <forever_fl@naver.com>
2024-03-11 18:06:04 +0900

    Modify count api

--- src/app/api/visitors/count/route.ts
+++ src/app/api/visitors/count/route.ts
@@ -1,18 +1,16 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { connectDB } from "@/lib/mongodb";
 import { Collection, Document } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "GET") {
-    res.status(405).json({ message: "Only GET requests are allowed" });
-    return;
-  }
+interface VisitDocument {
+  date: string;
+  visitors: string[];
+  count: number;
+}
 
+export async function GET(req: NextRequest) {
   const db = await connectDB();
-  const visits = db.collection("visits");
+  const visits = db.collection<VisitDocument>("visits");
 
   // 현재 날짜와 방문자 IP 주소를 구함
   const date = new Date().toISOString().split("T")[0]; // YYYY-MM-DD 형식
@@ -24,23 +22,26 @@ export default async function handler(
   // 총 방문자 수 조회
   const totalVisitors = await getTotalVisitors(visits);
 
-  res.status(200).json({ date, visitorIP });
+  // 총 방문자 수를 JSON으로 응답
+  return new NextResponse(JSON.stringify({ date, visitorIP, totalVisitors }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 // IP 주소를 추출하는 함수
-const getVisitorIP = (req: NextApiRequest) => {
-  const forwarded = req.headers["x-forwarded-for"];
-  const ip =
-    typeof forwarded === "string"
-      ? forwarded.split(",")[0]
-      : req.socket.remoteAddress;
+const getVisitorIP = (req: NextRequest) => {
+  const forwarded = req.headers.get("x-forwarded-for");
+  const ip = typeof forwarded === "string" ? forwarded.split(",")[0] : req.ip;
   return ip || "";
 };
 
 async function updateVisitorCount(
   date: string,
   visitorIP: string,
-  visits: Collection<Document>
+  visits: Collection<VisitDocument>
 ) {
   const filter = { date: date, visitors: { $ne: visitorIP } };
   const update = {
@@ -51,7 +52,7 @@ async function updateVisitorCount(
   await visits.updateOne(filter, update, options);
 }
 
-async function getTotalVisitors(visits: Collection<Document>) {
+async function getTotalVisitors(visits: Collection<VisitDocument>) {
   const result = await visits
     .aggregate([{ $group: { _id: null, total: { $sum: "$count" } } }])
     .toArray();
