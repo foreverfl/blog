0f19546531f4fc1e85cc0ff476ae436308600373
foreverfl <forever_fl@naver.com>
2024-04-06 07:04:43 +0900

    Resolve postprops error

--- src/app/post/ja/[post]/page.tsx
+++ src/app/post/ja/[post]/page.tsx
@@ -6,12 +6,14 @@ export default async function Page({ params }: { params: { post: string } }) {
   const postIdx = params.post;
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
   const response = await fetch(`${baseUrl}/api/post/${postIdx}`);
+  const post = await response.json();
+
   if (!response.ok) {
     return notFound();
   }
   return (
     <Layout postIdx={params.post}>
-      <Post postIdx={params.post} />
+      <Post postIdx={params.post} post={post} />
     </Layout>
   );
 }
