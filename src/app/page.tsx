49895264193cbee042affeaddb7c1322fba8b689
foreverfl <forever_fl@naver.com>
2024-06-30 20:59:43 +0900

    Add adsense script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Main from "../components/Main";
+import Script from "next/script";
 
 export default function Index() {
   return (
     <Layout postIdx={""}>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4751026650729929"
+        crossOrigin="anonymous"
+      />
       <Main />
     </Layout>
   );
