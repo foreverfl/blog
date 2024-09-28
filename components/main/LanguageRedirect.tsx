d199ada854ce1a2d9d2a9d0e3165cd939b290eb4
foreverfl <forever_fl@naver.com>
2024-09-28 09:56:31 +0900

    Add dynamic in 'mdxFiles.ts

--- components/main/LanguageRedirect.tsx
+++ components/main/LanguageRedirect.tsx
@@ -7,11 +7,11 @@ const LanguageRedirect = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const userLang = navigator.language.startsWith("ko") ? "ko" : "ja"; // 사용자의 언어 감지
-    router.replace(`/${userLang}`); // 언어에 따라 리다이렉트
+    const userLang = navigator.language.startsWith("ko") ? "ko" : "ja";
+    router.replace(`/${userLang}`);
   }, [router]);
 
-  return null; // 화면에는 아무것도 렌더링하지 않음
+  return null;
 };
 
 export default LanguageRedirect;
--- contents/mdxFiles.ts
+++ contents/mdxFiles.ts
@@ -1,17 +1,25 @@
-const mdxFiles: { [key: string]: React.ComponentType<any> } = {
+import dynamic from "next/dynamic";
+
+const mdxFiles: { [key: string]: any } = {
   // development/project
-  "ko/development/project/001-how-to-create-a-blog-with-nextjs":
-    require("@/contents/ko/development/project/001-how-to-create-a-blog-with-nextjs.mdx")
-      .default,
-  "ja/development/project/001-how-to-create-a-blog-with-nextjs":
-    require("@/contents/ja/development/project/001-how-to-create-a-blog-with-nextjs.mdx")
-      .default,
-  "ko/development/project/002-making-docs-site":
-    require("@/contents/ko/development/project/002-making-docs-site.mdx")
-      .default,
-  "ja/development/project/002-making-docs-site":
-    require("@/contents/ja/development/project/002-making-docs-site.mdx")
-      .default,
+  "ko/development/project/001-how-to-create-a-blog-with-nextjs": dynamic(
+    () =>
+      import(
+        "@/contents/ko/development/project/001-how-to-create-a-blog-with-nextjs.mdx"
+      )
+  ),
+  "ja/development/project/001-how-to-create-a-blog-with-nextjs": dynamic(
+    () =>
+      import(
+        "@/contents/ja/development/project/001-how-to-create-a-blog-with-nextjs.mdx"
+      )
+  ),
+  "ko/development/project/002-making-docs-site": dynamic(
+    () => import("@/contents/ko/development/project/002-making-docs-site.mdx")
+  ),
+  "ja/development/project/002-making-docs-site": dynamic(
+    () => import("@/contents/ja/development/project/002-making-docs-site.mdx")
+  ),
 };
 
 export default mdxFiles;
