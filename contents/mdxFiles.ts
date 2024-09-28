5960955b89ab42cc7243873663dcc53f45d0081f
foreverfl <forever_fl@naver.com>
2024-09-28 10:22:03 +0900

    Hydration test

--- contents/mdxFiles.ts
+++ contents/mdxFiles.ts
@@ -1,25 +1,21 @@
-import dynamic from "next/dynamic";
+// 파일 경로 배열
+const mdxFilePaths = [
+  "ko/development/project/001-how-to-create-a-blog-with-nextjs",
+  "ja/development/project/001-how-to-create-a-blog-with-nextjs",
+  "ko/development/project/002-making-docs-site",
+  "ja/development/project/002-making-docs-site",
+];
 
-const mdxFiles: { [key: string]: any } = {
-  // development/project
-  "ko/development/project/001-how-to-create-a-blog-with-nextjs": dynamic(
-    () =>
-      import(
-        "@/contents/ko/development/project/001-how-to-create-a-blog-with-nextjs.mdx"
-      )
-  ),
-  "ja/development/project/001-how-to-create-a-blog-with-nextjs": dynamic(
-    () =>
-      import(
-        "@/contents/ja/development/project/001-how-to-create-a-blog-with-nextjs.mdx"
-      )
-  ),
-  "ko/development/project/002-making-docs-site": dynamic(
-    () => import("@/contents/ko/development/project/002-making-docs-site.mdx")
-  ),
-  "ja/development/project/002-making-docs-site": dynamic(
-    () => import("@/contents/ja/development/project/002-making-docs-site.mdx")
-  ),
+// SSR용 MDX 파일 import 함수
+const importMDXFileSSR = (path: string) => {
+  return require(`@/contents/${path}.mdx`).default;
 };
+
+// 동적으로 import할 파일을 객체로 저장
+const mdxFiles: { [key: string]: any } = {};
+
+mdxFilePaths.forEach((filePath) => {
+  mdxFiles[filePath] = importMDXFileSSR(filePath);
+});
 
 export default mdxFiles;
