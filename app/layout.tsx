1d4322ffad787348cb4ec3b0049042515ff876dd
foreverfl <forever_fl@naver.com>
2025-05-31 15:04:36 +0900

    fix: remove Google Analytics and Vercel Analytics scripts from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import Footer from "@/components/main/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import "@/styles/globals.css";
-import { GoogleAnalytics } from "@next/third-parties/google";
-import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,36 +24,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="google-adsense-account" content="ca-pub-4751026650729929" />
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4751026650729929"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-1TE9HNR07B"
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-1TE9HNR07B');
-    `,
-          }}
-        />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class">
             <Navbar />
             <main>
               {children}
-              <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID!} />
-              <Analytics />
             </main>
             <Footer />
         </ThemeProvider>
