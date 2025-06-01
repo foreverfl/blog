dddf2d579bc2aaea66f3826bffe7134bf08e1be9
foreverfl <forever_fl@naver.com>
2025-06-02 07:15:07 +0900

    fix: update ESLint and Prettier configurations in package files and solve build error

--- app/api/hackernews/translate/route.ts
+++ app/api/hackernews/translate/route.ts
@@ -1,5 +1,8 @@
 import { POST as commonPOST } from "./[date]/route";
 
 export async function POST(req: Request) {
-  return commonPOST(req, { params: Promise.resolve({ date: undefined }) });
+  return commonPOST(req, {
+    params: Promise.resolve({ date: undefined }),
+    searchParams: new URL(req.url).searchParams,
+  });
 }
